6e08542d4bff9b8fd42e6d1f5a4a28cca3cb1c9a
syswu <wuheng@iscas.ac.cn>
2020-09-14 08:19:57 +0800

    more cases

--- src/test/java/com/github/kubesys/vsphere/VirtualMachineTest.java
+++ src/test/java/com/github/kubesys/vsphere/VirtualMachineTest.java
@@ -10,7 +10,12 @@ public class VirtualMachineTest extends VsphereClientTest {
 //		System.out.println(client.virtualMachines().getVMInfo("vm-12"));
 //		System.out.println(client.virtualMachines().getImageInfo("vm-13"));
 //		System.out.println(client.virtualMachines().stop("vm-43"));
+		System.out.println(client.virtualMachines().reset("vm-21"));
+		System.out.println(client.virtualMachines().reboot("vm-21"));
+		System.out.println(client.virtualMachines().delete("vm-21"));
+		System.out.println(client.virtualMachines().suspend("vm-21"));
 		System.out.println(client.virtualMachines().updateCPU("vm-21", 2));
+		System.out.println(client.virtualMachines().updateRAM("vm-21", 4096));
 	}
 }
 
