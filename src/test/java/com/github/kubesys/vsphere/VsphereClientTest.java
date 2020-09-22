1fd9fedcdc84cea992e7e542607eb9353d42e5bf
syswu <wuheng@iscas.ac.cn>
2020-09-22 10:55:08 +0800

    support http

--- src/test/java/com/github/kubesys/vsphere/VsphereClientTest.java
+++ src/test/java/com/github/kubesys/vsphere/VsphereClientTest.java
@@ -5,5 +5,6 @@ package com.github.kubesys.vsphere;
  */
 public class VsphereClientTest {
 	
-	public static VsphereClient client = new VsphereClient("133.133.135.35", "administrator@vsphere.test", "Onceas2020!234");
+	public static VsphereClient client = new VsphereClient("https", "133.133.135.35", 443, 
+			"administrator@vsphere.test", "Onceas2020!234", "6.7");
 }
