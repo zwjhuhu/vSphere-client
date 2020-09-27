9335b2a3a582784911ff5ae9957cd092ec633c10
wuyuewen <340629194@qq.com>
2020-09-27 20:03:01 +0800

    add resource pool test

---
+++ src/test/java/com/github/kubesys/vsphere/list/ListResourcePoolsInResourcePoolTest.java
@@ -0,0 +1,14 @@
+package com.github.kubesys.vsphere.list;
+
+import com.github.kubesys.vsphere.VsphereClientTest;
+
+/**
+ * Unit test for simple App.
+ */
+public class ListResourcePoolsInResourcePoolTest extends VsphereClientTest {
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println(getClient().virtualMachinePools().listResourcePoolsInPool("group-4"));
+	}
+}
+
