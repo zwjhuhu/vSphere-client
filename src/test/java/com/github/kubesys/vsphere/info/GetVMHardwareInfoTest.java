7ccecde81a8413687042ee42d7b38d01566b8f89
742362144 <742362144@qq.com>
2020-10-27 08:51:45 +0800

    update

--- src/test/java/com/github/kubesys/vsphere/info/GetVMHardwareInfoTest.java
+++ src/test/java/com/github/kubesys/vsphere/info/GetVMHardwareInfoTest.java
@@ -8,8 +8,8 @@ import com.github.kubesys.vsphere.VsphereClientTest;
 public class GetVMHardwareInfoTest extends VsphereClientTest {
 
 	public static void main(String[] args) throws Exception {
-		String cookie= "CastleSessionvsphere.local=_ec864fd67563a0be0f96c833805fcbd9; JSESSIONID=1BFEF1D84F81C18737284239E6C3D94F7EC8; VSPHERE-USERNAME=Administrator%40VSPHERE.LOCAL; VSPHERE-CLIENT-SESSION-INDEX=_427ee4b26b983f5c0bf70387d837f237";
-		System.out.println(getClient().virtualMachines().getVMHardwareInfo("cnetos1", cookie));
+		String cookie= "CastleSessionvsphere.local=_e7ff372370fc04ab0904760944cd5876; JSESSIONID=BA334307A40FE0368B1BE28399199F282EC3; VSPHERE-USERNAME=Administrator%40VSPHERE.LOCAL; VSPHERE-CLIENT-SESSION-INDEX=_f770943e0f10b75d878ca8a89077a4ad";
+		System.out.println(getClient().virtualMachines().getVMHardwareInfo("vcenter", cookie));
 	}
 
 }
--- src/test/java/com/github/kubesys/vsphere/info/GetVMTemplateHardwareInfoTest.java
+++ src/test/java/com/github/kubesys/vsphere/info/GetVMTemplateHardwareInfoTest.java
@@ -1,5 +1,6 @@
 package com.github.kubesys.vsphere.info;
 
+import com.github.kubesys.vsphere.VsphereClient;
 import com.github.kubesys.vsphere.VsphereClientTest;
 
 /**
@@ -8,8 +9,10 @@ import com.github.kubesys.vsphere.VsphereClientTest;
 public class GetVMTemplateHardwareInfoTest extends VsphereClientTest {
 
 	public static void main(String[] args) throws Exception {
-		String cookie= "CastleSessionvsphere.local=_ec864fd67563a0be0f96c833805fcbd9; JSESSIONID=1BFEF1D84F81C18737284239E6C3D94F7EC8; VSPHERE-USERNAME=Administrator%40VSPHERE.LOCAL; VSPHERE-CLIENT-SESSION-INDEX=_427ee4b26b983f5c0bf70387d837f237";
-		System.out.println(getClient().virtualMachines().getVMTemplateHardwareInfo("部署服务器", cookie));
+		//String cookie= "CastleSessionvsphere.local=_e7ff372370fc04ab0904760944cd5876; JSESSIONID=BA334307A40FE0368B1BE28399199F282EC3; VSPHERE-USERNAME=Administrator%40VSPHERE.LOCAL; VSPHERE-CLIENT-SESSION-INDEX=_f770943e0f10b75d878ca8a89077a4ad";
+		VsphereClient client=getClient();
+		String cookie=client.getCookie();
+		System.out.println(getClient().virtualMachines().getVMTemplateHardwareInfo("vcenter", cookie));
 	}
 
 }
