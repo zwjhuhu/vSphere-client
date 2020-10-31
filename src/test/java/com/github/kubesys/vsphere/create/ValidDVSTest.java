0c4b8545fb00bf2d8c36ec149b740830db34b024
742362144 <742362144@qq.com>
2020-10-31 21:40:58 +0800

    update

--- src/test/java/com/github/kubesys/vsphere/create/ValidDVSTest.java
+++ src/test/java/com/github/kubesys/vsphere/create/ValidDVSTest.java
@@ -1,5 +1,6 @@
 package com.github.kubesys.vsphere.create;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.github.kubesys.vsphere.VsphereClient;
 import com.github.kubesys.vsphere.VsphereClientTest;
 
@@ -10,6 +11,7 @@ public class ValidDVSTest extends VsphereClientTest {
 
 	public static void main(String[] args) throws Exception {
 		VsphereClient vClient = getClient();
+		JsonNode uuid = vClient.virtualMachines().searchUUID("DSwitch2", "DistributedVirtualSwitch", vClient.getCookie());
 		System.out.println(vClient.virtualMachines().validDVS("urn:vmomi:VmwareDistributedVirtualSwitch:dvs-348:c0d6264c-3d65-4183-b3f3-516f4cb5fee0", vClient.getCookie()));
 	}
 
