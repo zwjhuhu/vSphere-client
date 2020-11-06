58a8708f8046556ff2323f44360a85e6d40f85c2
syswu <wuheng@iscas.ac.cn>
2020-11-06 08:35:53 +0800

    update

--- src/main/java/com/github/kubesys/vsphere/VsphereClient.java
+++ src/main/java/com/github/kubesys/vsphere/VsphereClient.java
@@ -81,11 +81,7 @@ public class VsphereClient {
 		this.httpClient = createHttpClient(false);
 		this.session = new ObjectMapper().readTree(
 				simpleLogin(username, password).body().byteStream()).get("value").asText();
-		JsonNode json = virtualMachinePools().listNetworks();
-		if (json.has("type") && json.get("type")
-					.asText().contains("unauthenticated")) {
-			throw new Exception("unauthenticated errors, please check url, username, password");
-		}
+		virtualMachinePools().listNetworks();
 	}
 
 	/*******************************************************
--- src/test/java/com/github/kubesys/vsphere/VsphereClientTest.java
+++ src/test/java/com/github/kubesys/vsphere/VsphereClientTest.java
@@ -9,8 +9,8 @@ public class VsphereClientTest {
 
 	public static VsphereClient getClient() throws Exception {
 		if (client == null) {
-//			client = new VsphereClient("https", "133.133.135.35", 443, "administrator@vsphere.test", "Onceas2020!234", "6.7");
-			client = new VsphereClient("https", "192.168.96.128", 443, "administrator@vsphere.local", "Qwe@1234", "6.5");
+			client = new VsphereClient("https", "133.133.135.35", 443, "administrator@vsphere.test", "Onceas2020!2345", "6.7");
+//			client = new VsphereClient("https", "192.168.96.128", 443, "administrator@vsphere.local", "Qwe@1234", "6.5");
 		}
 		return client;
 	}
